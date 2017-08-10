c7d360b6830ad9b83d25dc110eee5e01615a3450
jarmokallio <jarmo.kalli@helsinki.fi>
2017-08-10 05:21:15 +0300

    tietokantajutut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,19 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Kysymys -taulun testidata
+
+INSERT INTO Kysymys (nimi, kysymysteksti, vastaustyyppi)
+ VALUES ('Pääaine','Kirjoita pääaineesi',1);
+
+INSERT INTO Vastaus (vastaaja_id,
+  vastausteksti) 
+ VALUES (11, 'gooby pls');
+
+INSERT INTO Käyttäjä (nimi,
+  käyttöoikeus)
+VALUES ('Dolan', 1);
+
+INSERT INTO Kyselylomake(julkaisuaika,sulkemisaika) VALUES (2000,2007);
+
+INSERT INTO Kurssi (nimi, laitos) VALUES ('johdatus filosofointiin', 'filosofisen filosofian ja sosiaalifilosofian laitos');
+
+INSERT INTO YksityiskohtainenRaportti(laatimisaika,raportti) VALUES (20000, 'Deep analysis follows:');
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,60 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kysymys(
+  kysymys_id SERIAL PRIMARY KEY,
+  nimi VARCHAR (100),
+  kysymysteksti VARCHAR (500),
+  vastaustyyppi INTEGER,
+  välin_alku INTEGER,
+  välin_loppu INTEGER
+
+);
+
+CREATE TABLE Vastaus(
+  kysymys_id INTEGER REFERENCES Kysymys(kysymys_id),
+  vastaaja_id INTEGER ,
+  vastausteksti VARCHAR (500),
+  num_vastaus INTEGER,
+  likert_vastaus INTEGER
+
+);
+
+CREATE TABLE Käyttäjä(
+  käyttäjä_id SERIAL PRIMARY KEY,
+  nimi VARCHAR (100),
+  käyttöoikeus INTEGER
+
+);
+
+CREATE TABLE Kyselylomake(
+  lomake_id SERIAL PRIMARY KEY,
+  julkaisuaika DATE,
+  sulkemisaika DATE
+
+);
+
+CREATE TABLE LiitosLomakeKysymys(
+  kysymys_id INTEGER REFERENCES Kysymys(kysymys_id),
+  kyselylomake_id INTEGER REFERENCES Kyselylomake(lomake_id)
+
+);
+
+CREATE TABLE Kurssi(
+  kurssi_id SERIAL PRIMARY KEY,
+  lomake_id INTEGER REFERENCES Kyselylomake(lomake_id),
+  nimi VARCHAR (100),
+  laitos VARCHAR (100)
+
+);
+
+CREATE TABLE LiitosKäyttäjäKurssi(
+  kurssi_id INTEGER REFERENCES Kurssi(kurssi_id),
+  käyttäjä_id INTEGER REFERENCES Käyttäjä(käyttäjä_id)
+
+);
+
+CREATE TABLE YksityiskohtainenRaportti(
+  laatija INTEGER REFERENCES Käyttäjä(käyttäjä_id),
+  laatimisaika DATE,
+  raportti VARCHAR (5000)
+
+);
